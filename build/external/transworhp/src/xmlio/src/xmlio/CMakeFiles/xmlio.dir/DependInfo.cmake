2084f2fb1fe25ba4375044cd46d0fbf6226d50e6
Luis <luis1@uni-bremen.de>
2020-03-19 20:28:03 +0100

    added build folder

---
+++ build/external/transworhp/src/xmlio/src/xmlio/CMakeFiles/xmlio.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/localdata/luis1/git/galileonautic/external/transworhp/src/xmlio/src/xmlio/conversion.cpp" "/localdata/luis1/git/galileonautic/build/external/transworhp/src/xmlio/src/xmlio/CMakeFiles/xmlio.dir/conversion.cpp.o"
+  "/localdata/luis1/git/galileonautic/external/transworhp/src/xmlio/src/xmlio/textout.cpp" "/localdata/luis1/git/galileonautic/build/external/transworhp/src/xmlio/src/xmlio/CMakeFiles/xmlio.dir/textout.cpp.o"
+  "/localdata/luis1/git/galileonautic/external/transworhp/src/xmlio/src/xmlio/xmlerror.cpp" "/localdata/luis1/git/galileonautic/build/external/transworhp/src/xmlio/src/xmlio/CMakeFiles/xmlio.dir/xmlerror.cpp.o"
+  "/localdata/luis1/git/galileonautic/external/transworhp/src/xmlio/src/xmlio/xmlnode.cpp" "/localdata/luis1/git/galileonautic/build/external/transworhp/src/xmlio/src/xmlio/CMakeFiles/xmlio.dir/xmlnode.cpp.o"
+  "/localdata/luis1/git/galileonautic/external/transworhp/src/xmlio/src/xmlio/xmlparser.cpp" "/localdata/luis1/git/galileonautic/build/external/transworhp/src/xmlio/src/xmlio/CMakeFiles/xmlio.dir/xmlparser.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../external/transworhp/src/xmlio/src/xmlio"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/external/transworhp/src/xmlio/src/xmlio/CMakeFiles/xmlio.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xmlio.dir/conversion.cpp.o"
+  "CMakeFiles/xmlio.dir/textout.cpp.o"
+  "CMakeFiles/xmlio.dir/xmlerror.cpp.o"
+  "CMakeFiles/xmlio.dir/xmlnode.cpp.o"
+  "CMakeFiles/xmlio.dir/xmlparser.cpp.o"
+  "libxmlio.pdb"
+  "libxmlio.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xmlio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
